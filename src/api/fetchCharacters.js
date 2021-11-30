6306fdd07f6aedaa2cb472c0f65ebbad6335b7c6
macmartinsRR <macmartins@runtime-revolution.com>
2021-11-30 15:25:52 +0000

    Created README

---
+++ src/api/fetchCharacters.js
@@ -0,0 +1,12 @@
+async function fetchCharacters(page) {
+  try {
+    const rmRes = await fetch(`${process.env.REACT_APP_URL}?page=${page}`);
+    const data = await rmRes.json();
+
+    return data;
+  } catch (err) {
+    throw err;
+  }
+}
+
+export default fetchCharacters;
--- src/api/index.js
+++ src/api/index.js
@@ -1,12 +1,2 @@
-async function fetchCharacters(page) {
-  try {
-    const rmRes = await fetch(`${process.env.REACT_APP_URL}?page=${page}`);
-    const data = await rmRes.json();
-
-    return data;
-  } catch (err) {
-    throw err;
-  }
-}
-
+import fetchCharacters from "./fetchCharacters";
 export { fetchCharacters };
